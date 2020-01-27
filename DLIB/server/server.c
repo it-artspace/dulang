3a0f0f260348e1cd235afd3242ce6e0befbef829
jernicozz <cendleader@gmail.com>
2020-01-27 23:34:52 +0300

    ik

--- DLIB/server/server.c
+++ DLIB/server/server.c
@@ -198,7 +198,7 @@ BIN_DECL(accept){
     int bread = 0;
     int read = 1;
     while(/*(strstr(rdbuf, "\r\n\r\n")==0*/ read){
-        read = recv(clfd, rdbuf+bread, 4096, 0);
+        read = recv(clfd, rdbuf+bread, 1, MSG_WAITALL);
         if( read < 0 )
             return 0;
         bread += read;
