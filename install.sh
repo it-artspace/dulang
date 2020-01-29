a28744a7aad07663203613f4db29fa85755abb9c
jernicozz <cendleader@gmail.com>
2020-01-29 05:53:28 +0300

    --

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@ mkdir ~/Dulang
 cp -R $(pwd) ~/Dulang
 echo "export PATH=$PATH:~/Dulang" >> ~/.bash_profile
 make &> /dev/null
+cd DLIB
+cd JSON && make && cd ..
+cd fs && make && cd ..
+cd ipc && make && cd ..
 echo "dulang installed succesfully"
